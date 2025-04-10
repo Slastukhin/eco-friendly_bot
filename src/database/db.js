aff8e6ec17c6048573c036da864bdc3dec61ea7d
Dima <dmitrii.slastukhin@tages.ru>
2025-04-10 17:53:28 +0300

    refactor: удален функционал баллов и тип отходов 'другое', упрощена логика утилизации

--- src/database/db.js
+++ src/database/db.js
@@ -10,13 +10,31 @@ const pool = new Pool({
 
 // Функции для работы с профилем
 async function getUserProfile(chatId) {
-    const result = await pool.query('SELECT * FROM newtable WHERE chat_id = $1', [chatId]);
+    const result = await pool.query('SELECT * FROM users WHERE chat_id = $1', [chatId]);
     return result.rows[0];
 }
 
 async function updateUserProfile(chatId, field, value) {
-    const query = `UPDATE newtable SET ${field} = $1 WHERE chat_id = $2 RETURNING *`;
-    const result = await pool.query(query, [value, chatId]);
+    let query;
+    let values;
+
+    // Преобразуем имя поля из интерфейса в имя поля в базе данных
+    const fieldMapping = {
+        'fio': 'fio',
+        'age': 'age',
+        'location': 'location',
+        'photo': 'photo_id'
+    };
+
+    const dbField = fieldMapping[field];
+    if (!dbField) {
+        throw new Error('Неверное имя поля');
+    }
+
+    query = `UPDATE users SET ${dbField} = $1 WHERE chat_id = $2 RETURNING *`;
+    values = [value, chatId];
+
+    const result = await pool.query(query, values);
     return result.rows[0];
 }
 
